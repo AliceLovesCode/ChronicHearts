679aef4c8f679190fd10367c357610c4e1280456
Alice Isabelle <alice@aliceisabelle.com>
2021-05-31 21:39:29 -0500

    Initial Commit

---
+++ Shared/ChronicHeartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronicHeartsApp.swift
+//  Shared
+//
+//  Created by Alice Isabelle on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct ChronicHeartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
